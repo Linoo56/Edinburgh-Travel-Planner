c06ac922aaf3123e5f06d4032461ceccb7c90953
Callum Lawson <46711152+calle594@users.noreply.github.com>
2019-03-04 13:25:36 +0000

    Add files via upload
    The main file is flaskblog.py, the others are templates needed for the pages functionality.
    The key thing needed to be added is a navbar, a login page ( which i hope to work on this week) and links between any other pages created, right now a homepage template exists and you can access it through http://localhost:5000/home and http://localhost:5000/register for the registration page. Im not sure how this will work on your machines with your MySQL database. I created a database called "myflaskapp"(the names of certain things need changed as i was following a guide to create this) with user "root" and password "test". i can create an account successfully and the error messages  work, the passwords and encrypted with Sha-256, although i am not sure how to check MySQL to see if the accounts created are stored there or not. But the database is there and ther is a connection between Flask and MySQL , i will discuss this in more detail in the meeting.

---
+++ flaskblog.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+import pymysql
+
+
+app = Flask(__name__)
+
+# Config MySQL
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='test',
+                             db='myflaskapp',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+class RegisterForm(Form): #Set min/max length of fields
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.hash(str(form.password.data))
+
+        # Create Cursor
+        cur = connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username,
+        password))
+
+        # Commit to DB
+        connection.commit()
+        #Close Connection
+        connection.close()
+        flash('You are now registered and can log in', 'success')
+
+        cur.close()
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
